be48a058122069f017f46cadccdb90e147ffc288
Choi woongsoon <dndtns1234@gmail.com>
2019-11-29 00:39:37 +0900

    modimodi

--- lab09/maze.js
+++ lab09/maze.js
@@ -1,36 +1,65 @@
+"use strict";
+
 /* CSE3026 : Web Application Development
  * Lab 09 - Maze
  */
-
 var loser = null;  // whether the user has hit a wall
 
 window.onload = function() {
+	var boundaries = $$("#maze div.boundary");
+	for (var i = 0; i < boundaries.length; i++) {
+			boundaries[i].onmouseover = overBoundary;
+	}
+	$("start").onclick = startClick;
+	$("end").onmouseover = overEnd;
+  $("maze").onmouseleave = overBody;
 
 };
 
-// called when mouse enters the walls; 
+// called when mouse enters the walls;
 // signals the end of the game with a loss
 function overBoundary(event) {
-	
+	var boundaries = $$("#maze div.boundary");
+	if (loser == 0){
+		for (var i = 0; i < boundaries.length; i++) {
+				//boundaries[i].addClassName("div.youlose");
+				boundaries[i].style.backgroundColor = "#ff8888";
+		}
+		$("status").innerHTML = "You lose! :(";
+	}
+	loser = 1;
 }
 
 // called when mouse is clicked on Start div;
 // sets the maze back to its initial playable state
 function startClick() {
-
+	loser = 0;
+	$("status").innerHTML = "Move your mouse over the 'S' to begin.";
+	var boundaries = $$("#maze div.boundary");
+	for (var i = 0; i < boundaries.length; i++) {
+			boundaries[i].style.backgroundColor = "#eeeeee";
+	}
 }
 
 // called when mouse is on top of the End div.
 // signals the end of the game with a win
 function overEnd() {
-
+	if (loser == 0){
+		$("status").innerHTML = "You win! :)";
+	}
+	loser = 1;
 }
 
 // test for mouse being over document.body so that the player
 // can't cheat by going outside the maze
 function overBody(event) {
-
+	var boundaries = $$("#maze div.boundary");
+	if (loser == 0){
+		for (var i = 0; i < boundaries.length; i++) {
+				//boundaries[i].addClassName("div.youlose");
+				boundaries[i].style.backgroundColor = "#ff8888";
+		}
+		$("status").innerHTML = "You lose! :(";
+	}
+	loser = 1;
 }
-
-
-
